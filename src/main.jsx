7765dd068c1a48c590d55f614df7913e5066a043
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-01-08 16:12:29 +0700

    mulai buat componnent navbar

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import App from './App'
 // import react router
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import './assets/css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
