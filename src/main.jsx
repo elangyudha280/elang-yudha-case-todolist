892f577c92fbd96a75043a8794ce195dc0d57727
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-01-08 15:48:33 +0700

    mulai fitur router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
+// import react router
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
 import './assets/css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
