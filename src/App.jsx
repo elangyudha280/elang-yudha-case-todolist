d3ec44552e6d04aac72073b2d20e5e8f9ef76fa2
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-01-08 16:19:16 +0700

    selesai desain navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 
+import { Outlet } from 'react-router-dom'
+
+// COMPONENT NAVBAR
+import Navbar from './component/navbar'
+
 function App() {
 
   return (
     <div className="App">
-      
+      <Navbar/>
+
+      <Outlet/>
     </div>
   )
 }
--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -3,7 +3,13 @@
 
 
 const Navbar = ()=>{
-    return
+    return (
+        <nav className="navbar bg-transparent navbar-dashboard fw-bold py-4">
+            <div className="container">
+                <a className="navbar-brand text-white" href="#">TODO LIST APP</a>
+            </div>
+        </nav>
+    )
 }
 
 
