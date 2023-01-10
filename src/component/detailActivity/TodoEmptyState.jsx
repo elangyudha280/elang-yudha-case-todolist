26f1d662e42c1d1e460d51fb76c28cd124a873ad
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-01-10 14:56:58 +0700

    fix event

--- src/component/detailActivity/TodoEmptyState.jsx
+++ src/component/detailActivity/TodoEmptyState.jsx
@@ -1,11 +1,13 @@
 
 import imgEmptyTodo from '../../assets/images/emptyTodo.png'
-
-
+import { useContext } from 'react';
+import { contextDetailActivity } from '../../context/ContextDetailActivity';
 
 const TodoEmptyState =()=>{
+    // data context detail activity
+    let {setCheckAddTodo} = useContext(contextDetailActivity)
     return (
-        <button className="todo-empty-state" data-cy="todo-empty-state">
+        <button className="todo-empty-state" onClick={()=>{setCheckAddTodo(true)}} data-cy="todo-empty-state">
             <img src={imgEmptyTodo} alt="No todo" />
         </button>
     )
