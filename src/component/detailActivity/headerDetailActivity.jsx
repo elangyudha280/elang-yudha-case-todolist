9c0cb1db51991b28ec53ee71e56bec6512b5efde
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-01-12 10:49:15 +0700

    selesai desain component dropdown sorting

--- src/component/detailActivity/headerDetailActivity.jsx
+++ src/component/detailActivity/headerDetailActivity.jsx
@@ -18,6 +18,9 @@ const HeaderDetailActivity = ()=>{
     // state untuk menyimpan data input title
     let [inputTitle,setInputTitle]= useState('')
 
+    // state untuk mengecheck dropdown sorting
+    let [dropdownSorting,setDropdownSorting] = useState(false)
+
     // function untuk det data detail title
     useEffect(()=>{
         setDetailTitle(detailActivity.title)
@@ -48,7 +51,10 @@ const HeaderDetailActivity = ()=>{
         }
     }
 
-
+    // event toggle dropdown sorting
+    let toggleDropdownSorting= ()=>{
+        (!dropdownSorting) ? setDropdownSorting(true) : setDropdownSorting(false)
+    }
 
     // event edit input title
     const editInputTitle = (e)=>{
@@ -78,7 +84,9 @@ const HeaderDetailActivity = ()=>{
                 </div>
             </form>
             <div className="button-action">
-                <button className="todo-sort-button" data-cy="todo-sort-button">
+                <button className="todo-sort-button" data-cy="todo-sort-button"
+                 onClick={toggleDropdownSorting}
+                >
                 <i className="bi bi-funnel"></i>
                 </button>
                 <button className="todo-add-button" onClick={()=>{
@@ -88,6 +96,38 @@ const HeaderDetailActivity = ()=>{
                         <i className="bi bi-plus-lg"></i>
                         Tambah
                 </button>
+                
+                {
+                    // cek untuk menampilkan dropdown sorting
+                    (dropdownSorting) &&
+                    <div className="devide-sorting">
+                    <button className="sort-selection" data-cy="sort-selection">
+                    <i className="bi bi-sort-down icon-sort"></i>
+                    <span className="title-sort">terbaru</span>
+                    <i className="bi bi-check-lg"></i>
+                    </button>
+                    <button className="sort-selection" data-cy="sort-selection">
+                    <i className="bi bi-sort-up icon-sort"></i>
+                    <span className="title-sort">terlama</span>
+                    <i className="bi bi-check-lg"></i>
+                    </button>
+                    <button className="sort-selection" data-cy="sort-selection">
+                    <i className="bi bi-sort-alpha-up icon-sort"></i>
+                    <span className="title-sort">A-Z</span>
+                    <i className="bi bi-check-lg"></i>
+                    </button>
+                    <button className="sort-selection" data-cy="sort-selection">
+                    <i className="bi bi-sort-alpha-up-alt icon-sort"></i>
+                    <span className="title-sort">Z-A</span>
+                    <i className="bi bi-check-lg"></i>
+                    </button>
+                    <button className="sort-selection" data-cy="sort-selection">
+                    <i className="bi bi-filter icon-sort"></i>
+                    <span className="title-sort">belum selesai</span>
+                    <i className="bi bi-check-lg"></i>
+                    </button>
+                    </div>
+                }
             </div>
         </header> 
     )
